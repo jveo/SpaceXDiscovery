01c2f34664eef0b7ca47cead00a06c4aede12abf
Jesse Viau <Jesse.viau92@stclairconnect.ca>
2023-10-05 15:13:58 -0700

    Implemented Base for hiking app project

---
+++ HikingProject/controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HikingProject
+//
+//  Created by Jesse viau on 2023-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
