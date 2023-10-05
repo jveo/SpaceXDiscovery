f985b32f163293b7879af5e7051832a54c32de09
Jesse Viau <Jesse.viau92@stclairconnect.ca>
2023-10-05 15:17:10 -0700

    Fix navigation between controllers

---
+++ HikingProject/controllers/AddTaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTaskViewController.swift
+//  HikingProject
+//
+//  Created by Jesse viau on 2023-10-05.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add Task"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
